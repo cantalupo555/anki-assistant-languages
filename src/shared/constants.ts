919b6aa82c8758045f0f0a8f2e5004e7db62b7db
cantalupo555 <cantalupo@protonmail.com>
2025-01-16 22:04:24 -0300

    feat: Add shared constants file for centralized configuration

---
+++ src/shared/constants.ts
@@ -0,0 +1,28 @@
+/**
+ * List of supported language codes in the system.
+ * Each code follows the "language-REGION" format (e.g., "en-US").
+ */
+export const supportedLanguageCodes = [
+    'en-US', // English (United States)
+    'it-IT', // Italian (Italy)
+    'de-DE', // German (Germany)
+    'fr-FR', // French (France)
+    'es-ES', // Spanish (Spain)
+    'pt-BR', // Portuguese (Brazil)
+    'nl-NL', // Dutch (Netherlands)
+    'pl-PL', // Polish (Poland)
+    'ru-RU', // Russian (Russia)
+    'cmn-CN', // Mandarin (China)
+    'ja-JP', // Japanese (Japan)
+    'ko-KR'  // Korean (Korea)
+];
+
+/**
+ * List of supported TTS (Text-to-Speech) services.
+ */
+export const supportedTTSServices = ['google', 'azure'];
+
+/**
+ * List of supported API services.
+ */
+export const supportedAPIServices = ['openrouter', 'google', 'anthropic'];
